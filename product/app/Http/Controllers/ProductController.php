4f87c37736fd1caaced2a2e8652b08a98f9c57ae
infrasoft <ariel@infrasoft.com.ar>
2023-11-12 20:35:32 -0300

    update controller and api routes

--- product/app/Http/Controllers/ProductController.php
+++ product/app/Http/Controllers/ProductController.php
@@ -7,6 +7,9 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
+    private $averageRating = 0.0;
+    private $images = [];
+    private $meetingPoint = "";
     /**
      * Display a listing of the resource.
      *
@@ -14,8 +17,18 @@ class ProductController extends Controller
      */
     public function index()
     {
-        $products = products::all();
-        return response()->json($products);
+        $products = json_decode(file_get_contents('https://jsoneditoronline.org/#left=cloud.baf82d0b0d6946f8a6b006fafb2a9293'), true);
+        dd($products);
+        if ($products) {
+        
+        foreach ($products['products'] as $product) {
+            if ($product['status'] === 'ACTIVE') {
+                $product = $this->parseProduct($product);
+
+                echo json_encode($product, JSON_PRETTY_PRINT);
+            }
+        }
+        }
     }
 
     /**
@@ -37,7 +50,7 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
-        $products = $this->model->NewInstance(); // verificar
+        $products = products::get($request); // verificar
         $products->fill($request->all());
         $this->validate($request, [
             "name"=> "required|string|max:1000"
@@ -54,7 +67,7 @@ class ProductController extends Controller
      */
     public function show($id)
     {
-        $products = $this->facade->findOrFail($id);
+        $products = products::get( $id );
         return response()->json($products);
     }
 
@@ -90,5 +103,77 @@ class ProductController extends Controller
     public function destroy($id)
     {
         //
+    }
+
+    public function setAverageRating(float $averageRating)
+    {
+        $this->averageRating = $averageRating;
+    }
+
+    public function getAverageRating()
+    {
+        return $this->averageRating;
+    }
+
+    public function setImages(array $images)
+    {
+        $this->images = $images;
+    }
+
+    public function getImages()
+    {
+        return $this->images;
+    }
+
+    public function setMeetingPoint(string $meetingPoint)
+    {
+        $this->meetingPoint = $meetingPoint;
+    }
+
+    public function getMeetingPoint()
+    {
+        return $this->meetingPoint;
+    }
+
+    public function toJSON()
+    {
+        return [
+            'averageRating' => $this->averageRating,
+            'images' => $this->images,
+            'meetingPoint' => $this->meetingPoint,
+        ];
+    }
+
+    private function parseProduct(array $product)
+    {
+        $averageRating = 0;
+        $reviewCountTotals = $product['reviews']['reviewCountTotals'];
+
+        for ($i = 1; $i <= 5; $i++) {
+            $averageRating += $reviewCountTotals[$i] * $i;
+        }
+
+        $averageRating = round($averageRating / array_sum($reviewCountTotals), 2);
+
+        $images = [];
+        foreach ($product['images'] as $image) {
+            $variants = $image['variants'];
+            $maxWidth = max(array_column($variants, 'width'));
+            $images[] = $variants[$maxWidth]['url'];
+        }
+
+        $meetingPoint = '';
+        $description = $product['description'];
+        if (strpos($description, 'Meeting point:') === 0) {
+            $meetingPoint = substr($description, 13);
+            $meetingPoint = ucfirst($meetingPoint);
+        }
+
+        $product = new products();
+        $product->setAverageRating($averageRating);
+        $product->setImages($images);
+        $product->setMeetingPoint($meetingPoint);
+
+        return $product;
     }
 }
